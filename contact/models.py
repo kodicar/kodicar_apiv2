8da73977ffe679f9e3d7d91b5a0a1bc82c2330fb
dom-inic <dominicnyambane22@gmail.com>
2021-03-13 16:48:22 +0300

    contact done

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    fname = models.CharField( max_length=50)
+    lname = models.CharField( max_length=50)
+    email = models.EmailField( max_length=254, unique = True)
+    subject = models.CharField( max_length=300)
+    message = models.TextField(max_length = 3000)
+    date_sent = models.DateTimeField( auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Contacts'
+
+    def __str__(self):
+        return self.email
+    
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . models import Contact
+from . serializers import ContactSerializer
+from rest_framework import generics 
 
 # Create your views here.
+
+class ContactView(generics.CreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    
