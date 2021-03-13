1ba34ebb3e3dd025b6dc0efb93cd9b2a7447c65f
dom-inic <dominicnyambane22@gmail.com>
2021-03-13 16:58:43 +0300

    reviews done

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cars.models import Cars
+from kodicar_api import settings
 
 # Create your models here.
+
+class Reviews(models.Model):
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Cars, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    title = models.CharField( max_length=50)
+    body = models.TextField(max_length=500)
+    created_at = models.DateTimeField( auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Reviews'
+
+    def __str__(self):
+        return self.title
+    
+
+    
+
+    
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from . models import Reviews
+from . serializers import ReviewSerializer
 
 # Create your views here.
+
+class ReviewsList(generics.ListCreateAPIView):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewSerializer
+
+    
