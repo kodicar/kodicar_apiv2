53be6e5503c756020b51aaec1f0bcbd94da38b9a
dom-inic <dominicnyambane22@gmail.com>
2021-03-15 23:41:19 +0300

    allowed hosts, debug set to false

--- kodicarapiv2/settings.py
+++ kodicarapiv2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ym0v+*uy#%qqxdxqt82ilgvev#@=@_os!a-*dl@s(jz$!8#z*9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kodicarmvpapi.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
