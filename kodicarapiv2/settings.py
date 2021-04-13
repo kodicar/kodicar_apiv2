ae58f1d00fdfbd94478fb8c6749413d55351c96b
dom-inic <dominicnyambane22@gmail.com>
2021-04-13 12:13:28 +0300

    debug to false

--- kodicarapiv2/settings.py
+++ kodicarapiv2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ym0v+*uy#%qqxdxqt82ilgvev#@=@_os!a-*dl@s(jz$!8#z*9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
