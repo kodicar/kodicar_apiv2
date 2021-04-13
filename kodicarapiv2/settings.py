303987e34d2ed4698e28affdb86ff21647fb7fe4
dom-inic <dominicnyambane22@gmail.com>
2021-04-13 13:23:33 +0300

    removed path

--- kodicarapiv2/settings.py
+++ kodicarapiv2/settings.py
@@ -153,9 +153,9 @@ STATICFILES_DIRS = (
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
-    "https://kodicarmvp.herokuapp.com/",
+    "https://kodicarmvp.herokuapp.com",
     "http://192.168.43.95:3000",
-    "http://localhost:5000/",
+    "http://localhost:5000",
 ]
 
 
