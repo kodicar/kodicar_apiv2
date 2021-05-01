656278771f4007e3a090d8c1f3844ca817b0a8a8
dom-inic <dominicnyambane22@gmail.com>
2021-05-01 11:56:55 +0300

    solving integrity error

--- cars/serializers.py
+++ cars/serializers.py
@@ -12,14 +12,7 @@ class CarSerializer(serializers.ModelSerializer):
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        fields = ("country", "street_address", "city", "county", "postal_code", "year", 
-                    "make", "model", "odometer", "colour", "mpg", "seats", "doors", "transmission", 
-                    "market_value", "vin", "license_plate", "price_per_day", "car_details", "description", 
-                    "as_from", "to", "guideline", "safety_and_quality_standards", "usb_charger", "gps", 
-                    "automatic", "sunroof", "apple_car_play", "all_wheel_drive", "bluetooth", "audio_input", 
-                    "convertible", "child_seat", "longterm_car", "bike_rack", "profile_photo", "mobile_number", 
-                    "drivers_license", "your_goals", "car_category"
-        )
+        fields = "__all__"
         exclude = ("date_added",)
 
 
