78419c0cbdda6314858673adc51e0efbc9ad4949
dom-inic <dominicnyambane22@gmail.com>
2021-05-04 17:47:40 +0300

    whitespace error debugged

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import UserCreate, LoginView, UserDetail
 urlpatterns = [
     path("users/", UserCreate.as_view(), name="user_create"),
     path("login/", LoginView.as_view(), name="login"),
-    path("user_detail/<int: pk>/", UserDetail.as_view(), name="user_detail"),
+    path("user_detail/<int:pk>/", UserDetail.as_view(), name="user_detail"),
     
     
 ]
