db20d96e99eacf8851ba38bac4e5d2275067b83a
dom-inic <dominicnyambane22@gmail.com>
2021-09-22 09:51:19 +0300

    allowed origins

--- kodicarapiv2/settings.py
+++ kodicarapiv2/settings.py
@@ -158,6 +158,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://www.kodicar.co.ke",
     "https://consharetest.herokuapp.com",
+    "https://www.conshare.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -165,7 +166,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.kodicar.co.ke",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://consharetest.herokuapp.com"
+    "https://consharetest.herokuapp.com",
+    "https://www.conshare.app",
+
 ]
 
 
